2621d0150cd7ae45b7a5e5cfb63a0a3da7c1640d
HSIN-HUNG, LI <tim131lee@gmail.com>
2025-08-16 10:08:24 +0800

    [style] format main.py; add blank lines for improved readability

--- src/pySNF/main.py
+++ src/pySNF/main.py
@@ -7,8 +7,10 @@ from FrameViewer.all_snfs import AllSNFsFrame
 from FrameViewer.prediction import PredictionFrame
 from io_file import load_dataset, get_stdh_path
 
+
 class SearchApp(tk.Tk):
     """Main window for SNFs Dataset Explorer App."""
+
     def __init__(self):
         super().__init__()
         self.title(
